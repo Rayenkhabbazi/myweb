f7aff154f0ff1d7b63151d6bcb39eac94ef48cf5
Rayen_khabbazi <123606520+Rayenkhabbazi@users.noreply.github.com>
2024-11-14 13:25:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    // Toggle the 'open' class on the menu and the hamburger icon
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
